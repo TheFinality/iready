34058566b9e156683066138c365ab29687a39b42
Jealousy <67923032+TheFinality@users.noreply.github.com>
2021-11-12 13:32:13 -0800

    Update new.js

--- new.js
+++ new.js
@@ -1,3 +1,14 @@
 var thingy = prompt("What do you want to do? \n 1 = pass lesson | 2 = start minute farming | 3 = end minute farming");
 var thing = parseInt(thingy)
-if (thing === 1) return alert('1')
+if (thing === 1) {
+  alert('1')
+  
+} else if (thing === 2) {
+  alert('2')
+  
+  
+} else if (thing === 3) {
+  alert('3')
+  
+  
+} else return alert('Your choice must be 1, 2, or 3!')
