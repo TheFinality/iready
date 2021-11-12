e382ab3455fbc7214d697b6a6a23f1fdcd499903
Jealousy <67923032+TheFinality@users.noreply.github.com>
2021-11-12 13:11:11 -0800

    Update new.js

--- new.js
+++ new.js
@@ -1,2 +1,3 @@
 var thingy = prompt("What do you want to do? \n 1 = pass lesson | 2 = start minute farming | 3 = end minute farming")
 if (thingy != 1 || thingy != 2 || thingy != 3) return alert('You must say 1, 2, or 3!')
+alert('what')
