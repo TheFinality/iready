7922930a7849a475199cdb5d8667ee1d2e9c05c4
Jealousy <67923032+TheFinality@users.noreply.github.com>
2021-11-12 13:34:50 -0800

    Update new.js

--- new.js
+++ new.js
@@ -1,14 +1,19 @@
+var script = document.createElement('script');
+script.setAttribute('src', 'https://npmcdn.com/sweetalert2@4.0.15/dist/sweetalert2.min.js');
+script.setAttribute('type', 'text/javascript');
+document.getElementsByTagName('head')[0].appendChild(script);
+
 var thingy = prompt("What do you want to do? \n 1 = pass lesson | 2 = start minute farming | 3 = end minute farming");
-var thing = parseInt(thingy)
+var thing = parseInt(thingy);
 if (thing === 1) {
-  alert('1')
+  alert('1');
   
 } else if (thing === 2) {
-  alert('2')
+  alert('2');
   
   
 } else if (thing === 3) {
-  alert('3')
+  alert('3');
   
   
-} else return alert('Your choice must be 1, 2, or 3!')
+} else return alert('Your choice must be 1, 2, or 3!');
